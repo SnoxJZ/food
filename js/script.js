6f1687f486304e58a000ec1165aebcad3b90a87c
Snox <snox.jz@gmail.com>
2024-10-30 19:23:02 +0200

    carousel dots

--- js/script.js
+++ js/script.js
@@ -297,6 +297,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // slider
     const slide = document.querySelectorAll('.offer__slide'),
+          sliderParent = document.querySelector('.offer__slider'),
           prevBtn = document.querySelector('.offer__slider-prev'),
           nextBtn = document.querySelector('.offer__slider-next'),
           currSlide = document.querySelector('#current'),
@@ -307,6 +308,76 @@ window.addEventListener('DOMContentLoaded', () => {
     let currentSlide = 0
     let offset = 0;
 
+
+    // Dots
+    sliderParent.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    sliderParent.append(indicators);
+
+    for (let i = 0; i < slide.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i);
+        dot.classList.add('dot');
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    function activeDot() {
+        dots.forEach(item => item.style.opacity = '.5');
+        dots[currentSlide].style.opacity = 1;
+    }
+    activeDot();
+
+    function delNotDigits(str) {
+        return +str.replace(/\D/ig, '');
+    }
+
+    dots.forEach(item => {
+        item.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            currentSlide = +slideTo;
+
+            offset = delNotDigits(width) * (slideTo);
+            slidesField.style.transform = `translateX(-${offset}px)`
+
+            activeDot();
+            slideCounter();
+        })
+    })
+    
+    
     // Карусель
     totalSlide.innerText = getZero(slide.length);
     function slideCounter() {
@@ -321,12 +392,12 @@ window.addEventListener('DOMContentLoaded', () => {
     slidesWrapper.style.overflow = 'hidden';
 
     slide.forEach(item => item.style.width = width);
-
+    
     nextBtn.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slide.length - 1)) {
+        if (offset == delNotDigits(width) * (slide.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += delNotDigits(width);
         }
 
         if(currentSlide < slide.length - 1) {
@@ -337,13 +408,15 @@ window.addEventListener('DOMContentLoaded', () => {
         slideCounter()
 
         slidesField.style.transform = `translateX(-${offset}px)`
+
+        activeDot();
     })
 
     prevBtn.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slide.length - 1);
+            offset = delNotDigits(width) * (slide.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= delNotDigits(width);
         }
 
         if(currentSlide > 0) {
@@ -354,6 +427,8 @@ window.addEventListener('DOMContentLoaded', () => {
         slideCounter()
 
         slidesField.style.transform = `translateX(-${offset}px)`
+
+        activeDot();
     })
 
 
@@ -387,7 +462,8 @@ window.addEventListener('DOMContentLoaded', () => {
     //     }
     //     hideSlide();
     //     showSlide(currentSlide);
-    //     slideCounter()
+    //     slideCounter();
+    //     activeDot();
     // })
 
     // nextBtn.addEventListener('click', () => {
@@ -398,6 +474,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //     }
     //     hideSlide();
     //     showSlide(currentSlide);
-    //     slideCounter()
+    //     slideCounter();
+    //     activeDot();
     // })
 })
